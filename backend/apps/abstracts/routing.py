e827ae53797941d26e2dcab3fd7869a79ae5943e
edescal <eduardo1582000@gmail.com>
2026-08-09 17:36:57 -0600

    AMMEND

--- backend/apps/abstracts/routing.py
+++ backend/apps/abstracts/routing.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    path("ws/pdf/<uuid:job_id>/", consumers.PDFGenerationConsumer.as_asgi()),
+    re_path(r"ws/pdf/(?P<job_id>[^/]+)/$", consumers.PDFGenerationConsumer.as_asgi()),
 ]
